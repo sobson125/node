f20e4e4d345fac958c1d9c506e379c3d6ce5ae7e
Grzegorz <grzegorz.sobczyk125@gmail.com>
2021-01-16 15:09:35 +0100

    Create separate modules, refactor to use callback pattern

---
+++ weather_app/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const forecast = (longitude, latitude, callback) => {
+  const url = 'http://api.weatherstack.com/current?access_key=d38f133b7a0328dcef484928d48f081c&query='+encodeURIComponent(longitude) +',' + encodeURIComponent(latitude);
+  request({url: url, json: true}, (error, response) => {
+    if (error) {
+      callback('Service not available', null);
+    } else if (response.body.error) {
+      callback(response.body.error.type, null);
+    } else {
+      const summary = `Temperatura: ${response.body.current.temperature}, odczuwalna: ${response.body.current.feelslike}`;
+      callback(null, summary);
+    }
+  });
+};
+
+module.exports = forecast;
