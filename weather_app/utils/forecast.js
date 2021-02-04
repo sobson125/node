1aaffabc0e37469f66d5c73b5c73cefbb794e944
Grzegorz <grzegorz.sobczyk125@gmail.com>
2021-02-04 21:52:32 +0100

    fix typo

--- weather_app/utils/forecast.js
+++ weather_app/utils/forecast.js
@@ -4,7 +4,7 @@ const forecast = async (longitude, latitude) => {
   const url = 'http://api.weatherstack.com/current?access_key=d38f133b7a0328dcef484928d48f081c&query='+encodeURIComponent(latitude) +',' + encodeURIComponent(longitude)+'&units=m';
   try {
     const response = await axios.get(url);
-    const summary = `Temperatura: ${response.data.current.temperature}, odczuwalna: ${response.data.current.feelslike}`;
+    const summary = `Temperature: ${response.data.current.temperature}, feels like: ${response.data.current.feelslike}`;
     return summary;
   } catch (e) {
     console.log(e);
